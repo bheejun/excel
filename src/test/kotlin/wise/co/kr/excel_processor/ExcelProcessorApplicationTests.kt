4a392fa0538005377391b5654035bec6ddc7f562
bheejun <bhjun95@gmail.com>
2024-07-02 23:49:22 +0900

    Initial Commit

---
+++ src/test/kotlin/wise/co/kr/excel_processor/ExcelProcessorApplicationTests.kt
@@ -0,0 +1,13 @@
+package wise.co.kr.excel_processor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExcelProcessorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
