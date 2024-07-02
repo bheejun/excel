cbcab0a48c40e0e4c55ca14120cc3cf7a9bf066c
bheejun <128471362+bheejun@users.noreply.github.com>
2024-07-02 23:47:16 +0900

    Initial Commit

---
+++ excel_processor/src/main/kotlin/wise/co/kr/excel_processor/ExcelProcessorApplication.kt
@@ -0,0 +1,11 @@
+package wise.co.kr.excel_processor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExcelProcessorApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExcelProcessorApplication>(*args)
+}
